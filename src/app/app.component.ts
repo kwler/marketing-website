7077d61af33ea770d450a7cd1b056b55fe78507b
Lyndon Bibera <lyndon.bibera@boltoninternational.com>
2018-09-02 21:05:38 +0800

    added way to close the warning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Kwler.NET Marketing Website';
+  showWarning = true;
+
+  hideWarning() {
+    this.showWarning = false;
+  }
 }
