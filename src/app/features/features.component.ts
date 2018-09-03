ee5db46ce85bb53368c574ffc5a99d059b4b6bf5
Lyndon Michael Bibera <lyndon.bibera@boltoninternational.com>
2018-09-03 17:32:51 +0800

    added basic "hackish" carousel

--- src/app/features/features.component.ts
+++ src/app/features/features.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeaturesComponent implements OnInit {
 
+  activeSlide: number = 0;
+  maxSlide: number = 3;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  next() {
+    console.log("next");
+    this.activeSlide++;
+    if (this.activeSlide==this.maxSlide) {
+      this.activeSlide = 0;
+    }
+  }
+
+  previous() {
+    console.log("previous");
+    this.activeSlide--;
+    if (this.activeSlide<0) {
+      this.activeSlide = this.maxSlide-1;
+    }
   }
 
 }
