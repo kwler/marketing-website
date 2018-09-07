2055c8cca0ef1155c244daeaf496269e741cffbb
Lyndon Bibera <lyndon.bibera@boltoninternational.com>
2018-09-07 17:57:47 +0800

    improved cards for tech

--- src/app/technologies/technologies.component.ts
+++ src/app/technologies/technologies.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TechnologiesComponent implements OnInit {
 
+  public techs = [
+    new TechCard("AWS", "Our cloud platform"),
+    new TechCard("GCP", "lol"),
+    new TechCard("Azure", "more lols")
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+class TechCard {
+
+  constructor(public name: string, public description: string) {}
 }
